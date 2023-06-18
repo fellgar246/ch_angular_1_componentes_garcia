f4389bf8d301392f1d0aad68f7b0183fff453aa7
fellgar246 <fellgar246@gmail.com>
2023-06-18 13:22:34 -0600

    content component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    MainWrapperComponent
+    ContentComponent
   ],
   imports: [
     BrowserModule,
